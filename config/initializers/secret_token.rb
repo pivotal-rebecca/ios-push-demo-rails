cbfceae3a18c91ac7446e296a7bde8f5b4e76046
Rebecca <h4xnoodle@gmail.com>
2014-02-05 21:30:41 -0500

    Rails + grocer demo for iOS Feb 5 seminar, Toronto office.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PusheenRails::Application.config.secret_key_base = '7128ef4c818d8935b3963c14f0a3f261c85e64ad22dec3921b849c06b42d38403dbdce9f01c5f6693de0065d0221b178a6471b19ca1fabd302979229b9166635'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PusheenRails::Application.config.session_store :cookie_store, key: '_Pusheen-rails_session'
